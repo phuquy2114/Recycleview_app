0756c24166663248021d3dbf5ec1a63c245153e6
uits <vnfdnservice@gmail.com>
2020-07-20 20:18:19 +0700

    detail player

--- app/src/main/java/com/uits/listrecycleview/detail/MusicPlayerActivity.kt
+++ app/src/main/java/com/uits/listrecycleview/detail/MusicPlayerActivity.kt
@@ -1,16 +1,25 @@
 package com.uits.listrecycleview.detail
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
 import com.uits.listrecycleview.R
 import kotlinx.android.synthetic.main.activity_detail_music.*
+
 
 /**
  * Music Player Activity
  * Copyright © 2020 UITS CO.,LTD
  * Created PHUQUY on 7/20/20.
  **/
-class MusicPlayerActivity : AppCompatActivity() {
+class MusicPlayerActivity : AppCompatActivity(), Runnable {
+
+    private lateinit var mMediaPlayer: MediaPlayer
+    private val mHandler: Handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +27,45 @@ class MusicPlayerActivity : AppCompatActivity() {
 
         var nameSong = intent.getStringExtra("name")
         mTvSong.setText(nameSong)
+
+        mMediaPlayer = MediaPlayer.create(this@MusicPlayerActivity, R.raw.tungchonhau)
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        mMediaPlayer.start()
+
+        seekBar.setMax(mMediaPlayer.duration / 1000);
+        seekBar()
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onProgressChanged(
+                seekBar: SeekBar,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                if (fromUser) {
+                    mMediaPlayer.seekTo(progress * 1000)
+                }
+            }
+        })
     }
 
     override fun onPause() {
         super.onPause()
+        if (mMediaPlayer.isPlaying) {
+            mMediaPlayer.stop()
+            mMediaPlayer.release()
+        }
+    }
+
+    fun seekBar() {
+        runOnUiThread(Runnable {
+            val mCurrentPosition = mMediaPlayer.currentPosition / 1000
+            seekBar.setProgress(mCurrentPosition)
+            mHandler.postDelayed(this, 1000);
+        })
+    }
+
+    override fun run() {
+        seekBar()
     }
 }
